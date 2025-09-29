011e94ad4d910a15c19db7aba162e666f36eed0b
StefanPenchev05 <comings.belts.0d@icloud.com>
2025-09-29 22:37:05 +0200

    add basename to Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }, [])
 
   return (
-     <Router>
+     <Router basename="/Teya-birthday-gift">
         <Routes>
           <Route path='/' element = {<HappyBirthday />}></Route>
           <Route path='/wishCard' element = {<WishCard />}></Route>
