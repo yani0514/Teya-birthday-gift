cdede1effd54d84202f95369eca9f362e8694a7c
StefanPenchev05 <comings.belts.0d@icloud.com>
2025-09-30 16:23:07 +0200

    refactor: remove unused state and clean up imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HappyBirthday from './components/HappyBirthday';
 import WishCard from './components/WishCard';
 import Wishes from './components/Wishes';
 
 function App() {
-  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     console.log("hui")
   }, [])
 
-  // Use basename only in production (when deployed to GitHub Pages)
-  const basename = import.meta.env.PROD ? "/Teya-birthday-gift" : "/";
-
   return (
-     <Router basename={basename}>
+     <Router basename={import.meta.env.PROD ? '/Teya-birthday-gift' : '/'}>
         <Routes>
           <Route path='/' element = {<HappyBirthday />}></Route>
           <Route path='/wishCard' element = {<WishCard />}></Route>
